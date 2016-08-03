d6a3582b0bf50e58971ecf281095d25c15e5b633
shubhamvasaikar <shubham.vasaikar7@gmail.com>
2016-08-03 17:13:56 +0530

    Add quality check step

---
+++ autotest/qualitychecks.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+from checklocales import locales as locales_list
+
+
+def extractPoFiles(program_name, locale_absent):
+    """ Generate all the .po files from the
+        .mo files.
+    """
+
+    # Remove absent locales obtained from checklocales.py
+    locales = [locale for locale in locales_list if locale not in locale_absent]
+    home_dir = os.path.expanduser('~')
+    output_dir = home_dir+"/.autotest/pofiles"  # Path to store .po files
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for locale in locales:
+        input_path = "/usr/share/locale/"+locale+"/LC_MESSAGES/"+program_name+".mo"
+        output_path = output_dir+"/"+locale+"."+program_name+".po"
+        command = "msgunfmt "+input_path+" > "+output_path
+        subprocess.call(command, shell=True)
+
+
+def runPofilter(program_name, locale_absent):
+    """ Run pofilter on all generated .po files.
+    """
+
+    locales = [locale for locale in locales_list if locale not in locale_absent]
+    home_dir = os.path.expanduser('~')
+    input_dir = home_dir+"/.autotest/pofiles"  # Path for .po files
+    output_dir = home_dir+"/.autotest/pofilter_files"
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for locale in locales:
+        input_path = input_dir+"/"+locale+"."+program_name+".po"
+        output_path = output_dir+"/filter."+locale+"."+program_name+".po"
+        command = "pofilter -i "+input_path+" -o "+output_path
+        subprocess.call(command, shell=True)
