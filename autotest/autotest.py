6b233425b6a55770c102721934ffaffad25c0283
shubhamvasaikar <shubham.vasaikar7@gmail.com>
2016-08-03 17:46:22 +0530

    Add pofilter quality check

--- autotest/autotest.py
+++ autotest/autotest.py
@@ -1,6 +1,8 @@
 import gi
+import subprocess
 import checkgtk
 import checklocales
+import qualitychecks
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
@@ -11,6 +13,10 @@ class Handler:
     def on_close(self, *args):
         Gtk.main_quit(*args)
 
+    # To open Nautilus window to view pofilter files.
+    def open_pofilter_dir(self, button):
+        subprocess.Popen("nautilus ~/.autotest/pofilter_files", shell=True)
+
     # Start program when the start button is clicked.
     def on_start(self, button):
         # builder.get_object used to get reference of gui objects.
@@ -18,7 +24,9 @@ class Handler:
         localeEntry = builder.get_object("locale")
         label0Entry = builder.get_object("label0")
         label1Entry = builder.get_object("label1")
-        
+        labelPofilter = builder.get_object("labelPofilter")
+        btnPofilter = builder.get_object("btnPofilter")
+
         # Set some frequently used variables
         program_name = prgmNameEntry.get_text()
         locale = localeEntry.get_text()
@@ -38,6 +46,11 @@ class Handler:
         else:
             locale_absent = ', '.join(locale_absent)
             label1Entry.set_text("These locales are not present: "+locale_absent)
+
+        qualitychecks.extractPoFiles(program_name, locale_absent)
+        qualitychecks.runPofilter(program_name, locale_absent)
+        labelPofilter.set_text("Pofilter run successfully.")
+        btnPofilter.set_sensitive(True)
 
 builder = Gtk.Builder()
 builder.add_from_file("autotest.glade")
