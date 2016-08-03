1ab15e0ef3b96226abdde69a256f1a8fd4da6533
shubhamvasaikar <shubham.vasaikar7@gmail.com>
2016-08-03 17:53:28 +0530

    Add commments.

--- autotest/qualitychecks.py
+++ autotest/qualitychecks.py
@@ -12,13 +12,16 @@ def extractPoFiles(program_name, locale_absent):
     locales = [locale for locale in locales_list if locale not in locale_absent]
     home_dir = os.path.expanduser('~')
     output_dir = home_dir+"/.autotest/pofiles"  # Path to store .po files
-
+    
+    # Create directory for .po files.
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
     for locale in locales:
         input_path = "/usr/share/locale/"+locale+"/LC_MESSAGES/"+program_name+".mo"
         output_path = output_dir+"/"+locale+"."+program_name+".po"
+
+        # msgunfmt is used to convert .mo files to .po files.
         command = "msgunfmt "+input_path+" > "+output_path
         subprocess.call(command, shell=True)
 
@@ -38,5 +41,8 @@ def runPofilter(program_name, locale_absent):
     for locale in locales:
         input_path = input_dir+"/"+locale+"."+program_name+".po"
         output_path = output_dir+"/filter."+locale+"."+program_name+".po"
+
+        # pofilter is obtained from translate-toolkit.
+        # pofilter is used to run various quality checks on .po files.
         command = "pofilter -i "+input_path+" -o "+output_path
         subprocess.call(command, shell=True)
