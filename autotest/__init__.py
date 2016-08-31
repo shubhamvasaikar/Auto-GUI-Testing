9645897f1e09075b2d8ebb103b0963ff45eeb02b
shubhamvasaikar <shubham.vasaikar7@gmail.com>
2016-08-31 12:44:35 +0530

    New log file everytime.

--- autotest/__init__.py
+++ autotest/__init__.py
@@ -2,5 +2,5 @@ import logging
 import logging.config
 
 formatter = "%(asctime)s - %(levelname)-6s: %(name)s: %(message)s"
-logging.basicConfig(filename="autotest.log", format=formatter, level=logging.NOTSET)
+logging.basicConfig(filename="autotest.log", filemode='w', format=formatter, level=logging.NOTSET)
 logging.getLogger(__name__)
