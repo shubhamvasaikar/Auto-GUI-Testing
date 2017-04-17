228266fa83b1b886b82d14ea68d5c9efc1efaa75
shubhamvasaikar <shubham.vasaikar7@gmail.com>
2017-04-17 14:16:13 +0530

    Create directory for saving images, clean images and check for invalid unicode.

--- autotest/extractwidgets.py
+++ autotest/extractwidgets.py
@@ -1,14 +1,61 @@
+# -*- coding: utf-8 -*-
+
 import pyatspiwrapper
 import os
+import getpass
+from PIL import Image
 
 global count
 count = 0
 
 
-def extractWidgets(app_name):
-    app = pyatspiwrapper.getAppReference(app_name)
-    os.chdir("/home/svasaika/.autotest/images/gedit/")
-    app = app[0]
+class ExtractWidgets:
+    def __init__(self, program_name, locale):
+        self.program_name = program_name
+        self.locale = locale
 
-    pyatspiwrapper.getWidgetLocations(app, count)
-    print "done."
+        self.user = getpass.getuser()
+        self.home = ""
+
+        if self.user != "root":
+            self.home = "/home/" + self.user
+        else:
+            self.home = "/root"
+
+        self.images_dir = self.home + "/.autotest/images/" + self.program_name
+
+        self.extracted_strings = self.home + "/.autotest/extracted_strings/" + self.program_name + "." + self.locale
+
+        if not os.path.exists(self.images_dir):
+            os.makedirs(self.images_dir)
+
+    def extractWidgets(self):
+        app = pyatspiwrapper.getAppReference(self.program_name)
+        os.chdir(self.images_dir)
+        app = app[0]
+
+        pyatspiwrapper.getWidgetLocations(app, count)
+        print "done."
+
+    def cleanImages(self):
+        os.chdir(self.images_dir)
+        images = os.listdir('./')
+        for image in images:
+            im = Image.open(image)
+            im = im.resize((im.width * 3, im.height * 3), Image.ANTIALIAS)
+            im = im.convert('L')
+            im = im.point(lambda x: 0 if x < 175 else 255, '1')
+            im.save(image)
+
+    def checkRendering(self):
+        with open(self.extracted_strings, 'r') as f:
+            app_strings = f.readlines()
+
+        bad_renders = []
+
+        for line in app_strings:
+            line = line.decode('utf-8')
+            if u'◌' in line:
+                bad_renders.append(line)
+
+        return bad_renders
